19c1f7492ebc0f0431616792f95d0ccdd3a5fb54
L <l@server.penyedaprasa.com>
2021-01-17 21:49:34 +0700

    edit struktur path API

--- routes/api.php
+++ routes/api.php
@@ -22,25 +22,25 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::group(['prefix' => 'v1'], function() {
 	
 	// jwt
-	Route::post('register', 'ApiController@register');
-	Route::post('login', 'ApiController@authenticate');
-	Route::get('open', 'ApiController@open');
-	Route::get('logout', 'ApiController@logout');
+	Route::post('register', 'API\ApiController@register');
+	Route::post('login', 'API\ApiController@authenticate');
+	Route::get('open', 'API\ApiController@open');
+	Route::get('logout', 'API\ApiController@logout');
 	
 	Route::group(['middleware' => ['jwt.verify']], function() {
-		Route::get('user', 'ApiController@getAuthenticatedUser');
-	    Route::get('closed', 'ApiController@closed');
-		Route::get('events', 'EventController@index');
-		Route::post('events/create', 'EventController@store');
-		Route::post('events/update', 'EventController@store');
-		Route::post('events/delete/{id}', 'EventController@delete');
-		Route::get('events/orders', 'OrderController@index');
-		Route::post('events/order/create', 'OrderController@store');
-		Route::post('events/order/delete/{code}', 'OrderController@delete');
-		Route::post('events/order/update', 'OrderController@update');
-		Route::get('users', 'UserController@index');
-		Route::post('users/profile/edit/{id}', 'UserController@editProfile');
-		Route::post('users/create', 'UserController@createUser');
+		Route::get('user', 'API\ApiController@getAuthenticatedUser');
+	    Route::get('closed', 'API\ApiController@closed');
+		Route::get('events', 'API\EventController@index');
+		Route::post('events/create', 'API\EventController@store');
+		Route::post('events/update', 'API\EventController@store');
+		Route::post('events/delete/{id}', 'API\EventController@delete');
+		Route::get('events/orders', 'API\OrderController@index');
+		Route::post('events/order/create', 'API\OrderController@store');
+		Route::post('events/order/delete/{code}', 'API\OrderController@delete');
+		Route::post('events/order/update', 'API\OrderController@update');
+		Route::get('users', 'API\UserController@index');
+		Route::post('users/profile/edit/{id}', 'API\UserController@editProfile');
+		Route::post('users/create', 'API\UserController@createUser');
 	});
 });
 
